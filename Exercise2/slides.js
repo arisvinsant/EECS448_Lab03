fcdc4edb9db4c73110449174337cf361825e5d63
Aris Vinsant <arisvinsant@ku.edu>
2019-02-27 17:32:52 -0600

    Updates to Exercise2

--- Exercise2/slides.js
+++ Exercise2/slides.js
@@ -1,15 +1,16 @@
+let imageArr = ["images/pic_1.jpeg", "images/pic_2.jpeg","images/pic_3.jpeg","images/pic_4.jpeg","images/pic_5.jpeg" ];
 let number = 1;
 function moveBack(){
     number = number - 1;
     if(number==0){
         number = 5;
     }
-    document.getElementById('slide').src = 'images/pic_' + number + '.jpeg';
+     document.getElementById("slide").src = imageArr[number];
 }
 function moveForward(){
     number = number + 1;
     if(number==6){
         number == 1;
     }
-    document.getElementById('slide').src = 'images/pic_' + number) + '.jpeg';
+     document.getElementById("slide").src = imageArr[number];
 }
